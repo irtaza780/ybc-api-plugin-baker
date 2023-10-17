577d5959c04d7e7ac82192f39947120d7686b20c
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-10-17 08:45:03 +0500

    fix: static otp for testing

--- src/util/generateOTPForResetPassword.js
+++ src/util/generateOTPForResetPassword.js
@@ -1,6 +1,8 @@
 export default async function generateOTPForResetPassword() {
   // Generate a random 6-digit number
-  const otp = Math.floor(1000 + Math.random() * 9000);
+  // const otp = Math.floor(1000 + Math.random() * 9000);
+
+  const otp = 1234
 
   // Set expiration time to 5 minutes from now
   const expirationTime = Date.now() + 15 * 60 * 1000; // in milliseconds
